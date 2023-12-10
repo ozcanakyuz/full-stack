9ffbe5b570e2b4a88ec7e6c853db6d5dea30450b
ozcanakyuz <xe_bilmece@hotmail.com>
2023-12-10 18:01:05 +0300

    content.html being edited
    content.html being edited

--- full_stack/full_stack/settings.py
+++ full_stack/full_stack/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#? upload
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- full_stack/home/urls.py
+++ full_stack/home/urls.py
@@ -6,5 +6,4 @@ from home import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('home/', views.index, name='index'),
-    path('admin/', admin.site.urls),
 ]
