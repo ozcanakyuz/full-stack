f9ceeef7806e9b14bbf19c6040887eb8a8baa75f
ozcanakyuz <xe_bilmece@hotmail.com>
2023-12-12 15:28:11 +0300

    login ok.
    login ok.

--- full_stack/home/urls.py
+++ full_stack/home/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('home', views.index, name='index'),
 
-    path('login', views.login_view, name='login_view'),
     path('logout',views.logout_view, name= 'logout_view'),
     # path('signup', views.signup_view, name='signup_view'),
 ]
--- full_stack/home/views.py
+++ full_stack/home/views.py
@@ -7,18 +7,8 @@ from django.shortcuts import render
 from home.forms import LoginForm
 from home.models import UserProfile
 
+#! LOG IN
 def index(request):
-    return render(request, 'index.html', {'page': 'Home'})
-
-@login_required(login_url='/login') # Check login
-def navbar(request):
-    current_user = request.user
-    profile = UserProfile.objects.get(user_id = current_user.pk)
-    context = {'profile': profile}
-    return render(request, 'navbar.html', context)
-
-#! LOG IN / LOG OUT / SIGN UP
-def login_view(request):
     if request.method == 'POST':  #check post
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -29,16 +19,46 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 messages.success(request, "You have successfully logged in {}".format(user.username))
-                return HttpResponseRedirect('/home')
+                return HttpResponseRedirect('/')
             else:
                 messages.warning(request, "Girilen Bilgiler Hatali Tekrar Deneyiniz {}".format(username))
                 return HttpResponseRedirect('/')
 
     form = LoginForm
-    context = {'form': form,} 
+    context = {'form': form,
+               'page': 'Home',} 
     return render(request, 'index.html', context)
 
-#* LOG OUT
+@login_required(login_url='/login') # Check login
+def navbar(request):
+    current_user = request.user
+    profile = UserProfile.objects.get(user_id = current_user.pk)
+    context = {'profile': profile}
+    return render(request, 'navbar.html', context)
+
+# def login_view(request):
+#     if request.method == 'POST':  #check post
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(username=username, password=password)
+
+#             if user is not None:
+#                 login(request, user)
+#                 messages.success(request, "You have successfully logged in {}".format(user.username))
+#                 return HttpResponseRedirect('/home')
+#             else:
+#                 messages.warning(request, "Girilen Bilgiler Hatali Tekrar Deneyiniz {}".format(username))
+#                 return HttpResponseRedirect('/')
+
+#     form = LoginForm
+#     context = {'form': form,} 
+#     return render(request, 'index.html', context)
+
+#! LOG OUT
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+#! SIGN-UP
