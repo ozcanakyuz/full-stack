a34e7b59736165b2f3d4b87889f1faa127479e33
ozcanakyuz <xe_bilmece@hotmail.com>
2023-12-21 11:37:16 +0300

    Sign Up Form Updated
    Added name and surname features to signup_form

--- full_stack/home/forms.py
+++ full_stack/home/forms.py
@@ -9,14 +9,16 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput())
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
     username = forms.CharField(max_length=30)
     email = forms.EmailField(max_length=30)
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields =('username','email','password')  # ['username', 'email']
-
+        fields =('first_name','last_name','username','email','password')
+        
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         #! Remove password fields from form
