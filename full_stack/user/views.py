cffc1c13540fa9374032de59c21a85364468f362
ozcanakyuz <xe_bilmece@hotmail.com>
2023-12-21 12:03:43 +0300

    Change Password Feature Fixed
    Password change feature added modal to user profile

--- full_stack/user/views.py
+++ full_stack/user/views.py
@@ -15,28 +15,32 @@ def index(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, request.FILES, instance=request.user)
         profile_form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
-        # formPassword = PasswordChangeForm(request.user, request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, 'Your account has been updated!')
-            return redirect('user_profile')
-        # elif formPassword.is_valid():
-        #     user = form.save()
-        #     update_session_auth_hash(request, user)  # Important!
-        #     messages.success(request, 'Your password was successfully updated!')
-        #     return HttpResponseRedirect('/user')
+        password_form = PasswordChangeForm(request.user, request.POST)
+        if 'update_submit' in request.POST:
+            if user_form.is_valid() and profile_form.is_valid():
+                user_form.save()
+                profile_form.save()
+                messages.success(request, 'Your account has been updated!')
+                return redirect('user_profile')
+        elif 'password_submit' in request.POST:
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)  # Important!
+                messages.success(request, 'Your password was successfully updated!')
+                return HttpResponseRedirect('/user')
+            else:
+                messages.error(request, 'Please correct the error below.<br>'+ str(password_form.errors))
+                return HttpResponseRedirect('/user')
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.userprofile)
-        # formPassword = PasswordChangeForm(request.user)
+        password_form = PasswordChangeForm(request.user)
         context = {'profile': profile,
                    'user_form': user_form,
                    'profile_form': profile_form,
-                #    'formPassword': formPassword,
+                    'password_form': password_form,
                    'page': 'USER PROFILE',}
         return render(request, 'user_profile.html', context)
-
 
 # @login_required
 # def user_settings(request):
